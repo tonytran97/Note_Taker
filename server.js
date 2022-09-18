3911dbb82672a70b399e3d0babea62019c8a8133
tonytran97 <tonytran_97@live.com>
2022-09-17 20:32:12 -0700

    post request failed

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const e = require('express');
 const express = require('express');
 const path = require('path');
 const db = require('./db/db.json');
 
+// helper method that generates unique IDs
+const uuid = require('./helper/uuid');
+
 const app = express();
 const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -18,5 +25,34 @@ app.get('/api/notes', (req, res) => res.json(db));
 app.get('*', (req, res) => 
 res.sendFile(path.join(__dirname, 'public/index.html')));
 
+// post request to save new notes
+app.post('/api/notes', (req, res) => {
+    res.json(`${req.method} request received`);
+
+    // the post request is logged into the terminal when the save button is clicked
+    console.info(`${req.method} request received, a note has been created`);
+
+    // destructuring of the items in req.body
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json(`Error in creating a new note`);
+    }
+})
+
 app.listen(PORT, () => 
-console.log(`Test app listening at http://localhost:${PORT}`));
+console.log(`Note app listening at http://localhost:${PORT}`));

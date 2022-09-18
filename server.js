93ec16572b6f0c776f80db712819d3eb887b2c73
tonytran97 <tonytran_97@live.com>
2022-09-17 19:22:45 -0700

    get request for apinotes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const db = require('./db/db.json');
 
 const app = express();
 const PORT = 3001;
@@ -8,11 +9,14 @@ app.use(express.static('public'));
 
 // get request for /notes will send the notes.html file
 app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, 'public/notes.html')))
+res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+// get request for notes that sends back a JSON file
+app.get('/api/notes', (req, res) => res.json(db));
 
 // sends the index.html file for when there are no matches to the requests above this
 app.get('*', (req, res) => 
-res.sendFile(path.join(__dirname, 'public/index.html')))
+res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.listen(PORT, () => 
 console.log(`Test app listening at http://localhost:${PORT}`));
